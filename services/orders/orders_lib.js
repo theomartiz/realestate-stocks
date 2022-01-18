07f551b55ebe7fc535678f2dca7cfd39c13b9418
Lxkx <lukatrailovic@live.fr>
2022-01-19 00:56:13 +0100

    service orders CRUD

---
+++ services/orders/orders_lib.js
@@ -0,0 +1,88 @@
+const fs = require("fs");
+const db = require("./db.json");
+
+const createOrder = async (newOrder) => {
+    
+    var order = {
+        id : getFirstAvailableId(db),
+        orderDate: newOrder.orderDate,
+        userId: newOrder.userId,
+        orderId: newOrder.orderId,
+        walletId: newOrder.walletId,
+        amount: newOrder.amount,
+    }
+
+    db.orders.push(order);
+
+    var data = JSON.stringify(db, null, 2);
+    fs.writeFile("db.json", data, finished);
+
+    function finished(){
+        console.log("db orders updated")
+    }
+}
+
+const getAllOrders = async () => {
+    return db.orders;
+}
+
+const getOrderById = async (id) => {
+    return db.orders.find((order) => order.id == id);
+}
+
+const updateOrderById = async (newOrder, oldOrderId) => {
+    
+    var orderToUpdateIndex = db.orders.findIndex((proj) => proj.id == oldOrderId);
+
+    if(orderToUpdateIndex != -1){
+        
+        var order = {
+            id : parseInt(oldOrderId),
+            orderDate: newOrder.orderDate,
+            userId: newOrder.userId,
+            orderId: newOrder.orderId,
+            walletId: newOrder.walletId,
+            amount: newOrder.amount,
+        }
+        db.orders.splice(orderToUpdateIndex, 1, order);
+
+        var data = JSON.stringify(db, null, 2);
+        fs.writeFile("db.json", data, finished);
+    }else{
+        return -1;
+    }
+   
+    function finished(){
+        console.log("order with id: " + order.id + " has been updated.");
+    }
+}
+
+const removeOrderById = async (id) => {
+    
+    var orderIndex = db.orders.findIndex((order) => order.id == id);
+    if(orderIndex >= 0){
+        db.orders.splice(orderIndex, 1);
+        var data = JSON.stringify(db, null, 2);
+        fs.writeFile("db.json", data, finished);     
+    }else{
+        return -1;
+    }
+   
+    function finished(){
+        console.log("order with id: " + id + " removed from database.");
+    }
+
+}
+
+function getFirstAvailableId(obj){
+    return  Object.keys(obj.orders).length + 1;
+}
+
+module.exports = 
+{
+    createOrder,
+    getAllOrders,
+    getOrderById,
+    removeOrderById,
+    updateOrderById
+}
--- services/orders/server.js
+++ services/orders/server.js
@@ -1,8 +1,16 @@
 const koa = require('koa');
 const koaRouter = require('@koa/router');
 
+const db = require("./db.json");
+const fs = require("fs");
+const { createOrder, getAllOrders, getOrderById, removeOrderById, updateOrderById } = require('./orders_lib.js');
+
 let router = koaRouter();
 let app = new koa();
+const BASE_URL = "/api/orders"
+
+const bodyParser  = require('koa-bodyparser');
+app.use(bodyParser());
 
 app.use(async (ctx,next) => {
     const start = new Date;
@@ -11,18 +19,57 @@ app.use(async (ctx,next) => {
     console.log('%s %s - %sms', ctx.method, ctx.url, ms);
 });
 
-router.get('/api/wallets', async (ctx) => {
-    ctx.body = "List of users...";
+
+router.get(BASE_URL, async (ctx) => {
+    ctx.body = await getAllOrders();
+});
+
+router.get(BASE_URL + '/:orderId', async (ctx) => {
+    
+    var id = ctx.params.orderId.replace(":", "");
+    var order = await getOrderById(id);
+    
+    if(order != null){
+        ctx.body = order;
+    }else{
+        ctx.body = "No order found with such ID";
+        ctx.status = 406;
+    }
 });
 
 
-router.get('/api/', async (ctx) => {
-    ctx.body = "API ready to receive requests";
+router.post(BASE_URL, async (ctx) => {
+    await createOrder(ctx.request.body);
+    ctx.body = "The following object has been added to the database : \n" + JSON.stringify(ctx.request.body, null, 2);
 });
 
-router.get('/', async (ctx) => {
-    ctx.body = "Ready to receive requests";
+router.put(BASE_URL + '/:orderId', async  (ctx) =>{
+    
+    var newOrder = ctx.request.body;
+    var id = ctx.params.orderId.replace(":", "");
+    var result = await updateOrderById(newOrder, id);
+    
+    if(result != -1){      
+        ctx.body = "Order with id: " + id + " has been updated."
+    }else{
+        ctx.body = "No order found with such ID";
+        ctx.status = 406;
+    }
 });
+
+router.delete(BASE_URL + '/:orderId', async (ctx) => {
+    //To get the param by removing the ":"
+    var id = ctx.params.orderId.replace(":", "");
+    var orderToRemove = db.orders.find((order) => order.id == id);
+    if(orderToRemove != null){
+        await removeOrderById(id);
+        ctx.body = "Order with id: " + id + " removed from database"
+    }else{
+        ctx.body = "No order found with such ID";
+        ctx.status = 406;
+    }
+});  
+
 
 app.use(router.routes()).use(router.allowedMethods());
 
